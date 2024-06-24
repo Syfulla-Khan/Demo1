e6ba72e9bd139385ae1035a68dea61afcaf5a159
Syfulla <spattan@dataeconomy.io>
2024-06-24 14:56:25 +0530

    Delecting unwanted files

--- test_fibonacci_test_cases.py
+++
@@ -1,9 +0,0 @@
-# Test Cases for Fibonacci function
-def test_Fibonacci():
-    assert Fibonacci(0) == 0
-    assert Fibonacci(1) == 1
-    assert Fibonacci(2) == 1
-    assert Fibonacci(3) == 2
-    assert Fibonacci(4) == 3
-    assert Fibonacci(5) == 5
-    assert Fibonacci(-1) == 'Incorrect input'
