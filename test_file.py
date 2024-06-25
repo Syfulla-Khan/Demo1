0447d1edf6b9528363b2c4aef13ba37793ff4a88
Syfulla <spattan@dataeconomy.io>
2024-06-25 11:49:02 +0530

    Add test cases file

---
+++ test_file.py
@@ -0,0 +1,10 @@
+# Basic Test Cases
+- Test case 1: Test for input 0
+- Test case 2: Test for negative input
+- Test case 3: Test for input 1
+- Test case 4: Test for input 2
+- Test case 5: Test for input greater than 2
+
+# Edge Test Cases
+- Test case 1: Test for large negative input
+- Test case 2: Test for large positive input
